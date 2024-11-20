d0519c7337e8bbd3b0026e05b8536b1382fbc689
Archit Rathod <166183458+K3NLYY@users.noreply.github.com>
2024-11-20 18:09:07 +0530

    Update fix.bat

--- fix.bat
+++ fix.bat
@@ -10,13 +10,13 @@ if %errorlevel%==1 ( goto :delete ) else exit
 echo please wait deleting cache...
 del %localappdata%\EADesktop\cache\qmlcache\*.*
 del %localappdata%\EALaunchHelper\cache\qmlcache\*.*
-del %localappdata%\Electronic Arts\EA Desktop\IGOCache\*.*
+del %localappdata%\Electronic Arts\EA Desktop\IGOCache
 del %localappdata%\Link2EA\cache\qmlcache\*.*
 del "%USERPROFILE%\documents\Need For Speed(TM) Unbound\cache\*.PcDx12*"
 
 echo.
 echo cache has been cleared downloading EA installer...
-set eaLink= "https://github.com/K3NLYY/NFS-Unbound-not-launching-fix/raw/refs/heads/main/NFSUB_Not_Launching_Fix_script/EAappInstaller.exe"
+set eaLink= "https://origin-a.akamaihd.net/EA-Desktop-Client-Download/installer-releases/EAappInstaller.exe"
 powershell -Command "Invoke-WebRequest -Uri '%eaLink%' -UseBasicParsing | Select-Object -ExpandProperty Content | Out-File -FilePath EAappInstaller.exe"
 
 echo.
